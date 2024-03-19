542a0e1e36e9780f724efde4a6876488375ff0cc
Thiago Recetto Moraes <89816163+ThFenixXD@users.noreply.github.com>
2024-03-19 09:00:55 -0300

    Upload arquivos do projeto

---
+++ JavaScript/script.js
@@ -0,0 +1,20 @@
+document.querySelector("#btn_sobreMim").addEventListener('click', () => {
+    window.location.href = "index.html";
+});
+
+document.querySelector("#btn_meusTrabalhos").addEventListener('click', () => {
+    window.location.href = "portfolio.html";
+});
+
+document.querySelector("#btn_minhaFormacao").addEventListener('click', () => {
+    window.location.href = "formacao.html";
+});
+
+document.querySelector("#btn_contato").addEventListener('click', () => {
+    window.location.href = "contato.html";
+});
+
+document.querySelector("#btn_enviar").addEventListener('click', () => {
+    alert('Enviado com Sucesso!');
+});
+
